c9ca9f3826e526ed842b44cedc4aee06e201adfc
1F10230116 <s1f102301167@iniad.org>
2025-12-03 17:58:20 +0900

    footerにアンケートリンクの挿入

--- src/features/layout/Footer.tsx
+++ src/features/layout/Footer.tsx
@@ -1,10 +1,32 @@
-import './Footer.css';
+import "./Footer.css";
 
 export const Footer = () => {
   const currentYear = new Date().getFullYear();
+  // 作成したGoogleフォームのURL
+  const formUrl =
+    "https://docs.google.com/forms/d/e/1FAIpQLSeFUdYm3c2w7CDRWJhgOuDTPonC7UR_yv_V8bOuDRyZOs21nQ/viewform?usp=header";
+
   return (
     <footer className="site-footer">
-      <p>&copy; {currentYear} 清水 俊也/Shimizu Shunya.</p>
+      <div className="footer-content">
+        <div className="survey-container">
+          <p className="survey-message">
+            ▼ ポートフォリオの評価実験にご協力お願いします ▼
+          </p>
+          <a
+            href={formUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="survey-button"
+          >
+            アンケートに回答する
+          </a>
+        </div>
+
+        <p className="copyright">
+          &copy; {currentYear} Shunya Shimizu. All Rights Reserved.
+        </p>
+      </div>
     </footer>
   );
 };
